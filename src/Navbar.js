5ca4510e8b373aa18c48a7f6640988a804bf9acc
Vivid <isaac.einn@gmail.com>
2022-05-21 19:32:28 +0800

    initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='logo'>
+            React
+        </div>
+        <navbar>
+            <a>About</a>
+            <a>Docs</a>
+            <a>Contact Me</a>
+        </navbar>
+    </div>
+  )
+}
